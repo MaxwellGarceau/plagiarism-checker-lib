c814ed7a6252a1ff1b98722cebaeeb9d1636b9be
Max Garceau <maxwellgarceau@gmail.com>
2024-12-16 15:41:03 -0500

    tests: Setup testing library

---
+++ tests/example.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+
+// TODO: Delete this after tests are added
+describe('Sample Test', () => {
+  it('always passes', () => {
+    expect(true).toBe(true);
+  });
+});
