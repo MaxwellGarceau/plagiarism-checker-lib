aaf09b3e36faa00ff98fe8a74b6253e24301ae98
Max Garceau <maxwellgarceau@gmail.com>
2025-01-12 21:32:56 -0500

    Update interface to include addDocuments

--- src/interfaces/TFIDFExtractor.ts
+++ src/interfaces/TFIDFExtractor.ts
@@ -3,14 +3,20 @@
  */
 interface TFIDFExtractor {
 	/**
-	 * Calculate TF-IDF for a term across all documents
+	 * Adds new documents to the corpus and updates internal calculations.
+	 * @param newDocuments An array of new documents to add
+	 */
+	addDocuments(newDocuments: string[]): void;
+
+	/**
+	 * Calculate TF-IDF for a term across all documents.
 	 * @param term The term to calculate TF-IDF for
 	 * @returns An array of TF-IDF values for the term in each document
 	 */
 	calculateTFIDF(term: string): { documentIndex: number; measure: number }[];
 
 	/**
-	 * Get the top terms for a specific document
+	 * Get the top terms for a specific document.
 	 * @param documentIndex Index of the document to analyze
 	 * @param topN Number of top terms to retrieve
 	 * @returns An array of terms with their TF-IDF scores
