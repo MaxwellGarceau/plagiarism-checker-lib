2b6ccacc3932411c30668e9b5138e70d8c2d246d
MaxwellGarceau <maxwellgarceau@gmail.com>
2025-01-14 11:23:39 -0500

    Enforce tab indentation in linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,19 +4,23 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-  },
-  {
-    ignores: ["node_modules/**", "dist/**", "coverage/**"],
-  },
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser, ...globals.node,
-      }
-    }
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+	{
+		files: ["**/*.{js,mjs,cjs,ts}"],
+	},
+	{
+		ignores: ["node_modules/**", "dist/**", "coverage/**"],
+	},
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser, ...globals.node,
+			}
+		},
+		rules: {
+			// Enforce tabs
+			"@/indent": ["error", "tab"],
+		},
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
 ];
