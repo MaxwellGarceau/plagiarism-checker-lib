2472987eb18365e7e1ac6456c6a272e308cf3da5
MaxwellGarceau <maxwellgarceau@gmail.com>
2025-01-14 12:26:45 -0500

    Fix lint errors

--- tests/process.test.ts
+++ tests/process.test.ts
@@ -5,42 +5,42 @@ import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
 describe('process', () => {
-  it('should tokenize text, remove stop words and punctuation, and lemmatize words', () => {
+	it('should tokenize text, remove stop words and punctuation, and lemmatize words', () => {
 
-    // Test input with stop words, punctuation and words to lemmatize
-    const input = readFileSync(resolve(__dirname, './__fixtures/processing/raw.txt'), 'utf-8');
+		// Test input with stop words, punctuation and words to lemmatize
+		const input = readFileSync(resolve(__dirname, './__fixtures/processing/raw.txt'), 'utf-8');
     
-    // Process the text
-    const result = process(input);
+		// Process the text
+		const result = process(input);
 
-	appLogger.info(result);
+		appLogger.info(result);
 
-    // Expected output after processing:
-    // - No stop words (the, are, over, the)
-    // - No punctuation (!)
-    // - Lemmatized words (foxes -> fox, jumping -> jump)
-    const expected = readFileSync(resolve(__dirname, './__fixtures/processing/expected.txt'), 'utf-8');
+		// Expected output after processing:
+		// - No stop words (the, are, over, the)
+		// - No punctuation (!)
+		// - Lemmatized words (foxes -> fox, jumping -> jump)
+		const expected = readFileSync(resolve(__dirname, './__fixtures/processing/expected.txt'), 'utf-8');
 
-    expect(result).toBe(expected);
-  });
+		expect(result).toBe(expected);
+	});
 
-  it('should convert numbers to words', () => {
-    const input = '1 2 3 4 5 6 7 8 9 0 1st 2nd 3rd 4th 5th 6th 7th 8th 9th 10th';
-    const result = process(input);
-    expect(result).toBe('one two three four five six seven eight nine zero first second third fourth fifth sixth seventh eighth ninth tenth');
-  });
+	it('should convert numbers to words', () => {
+		const input = '1 2 3 4 5 6 7 8 9 0 1st 2nd 3rd 4th 5th 6th 7th 8th 9th 10th';
+		const result = process(input);
+		expect(result).toBe('one two three four five six seven eight nine zero first second third fourth fifth sixth seventh eighth ninth tenth');
+	});
 
-  // TODO: Implement better normalization for abbreviations, slang, and acronyms
-  it.skip('should normalize abbreviations, slang, and acronyms', () => {
-    const input = 'I\'m gonna go to the store and get some groceries. See u there! Btw, what do u want 4 dinner?';
-    const result = process(input);
-    expect(result).toBe('i is going to go store get grocery see you there by the way what do you want for dinner');
-  });
+	// TODO: Implement better normalization for abbreviations, slang, and acronyms
+	it.skip('should normalize abbreviations, slang, and acronyms', () => {
+		const input = 'I\'m gonna go to the store and get some groceries. See u there! Btw, what do u want 4 dinner?';
+		const result = process(input);
+		expect(result).toBe('i is going to go store get grocery see you there by the way what do you want for dinner');
+	});
 
-  // TODO: Implement better spelling correction
-  it.skip('should fix spelling errors', () => {
-    const input = 'I\'m gokna go to the stere and get some grocaries.';
-    const result = process(input);
-    expect(result).toBe('i is going to go store get grocery');
-  });
+	// TODO: Implement better spelling correction
+	it.skip('should fix spelling errors', () => {
+		const input = 'I\'m gokna go to the stere and get some grocaries.';
+		const result = process(input);
+		expect(result).toBe('i is going to go store get grocery');
+	});
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    globals: true, // Enable global test methods like `describe` and `it`
-    environment: 'node', // Set the test environment (e.g., 'jsdom' for browser-like testing)
-    coverage: {
-      provider: 'v8', // default
-      reporter: ['text', 'lcov'], // Generate text and lcov coverage reports
-    },
-  },
+	test: {
+		globals: true, // Enable global test methods like `describe` and `it`
+		environment: 'node', // Set the test environment (e.g., 'jsdom' for browser-like testing)
+		coverage: {
+			provider: 'v8', // default
+			reporter: ['text', 'lcov'], // Generate text and lcov coverage reports
+		},
+	},
 });
