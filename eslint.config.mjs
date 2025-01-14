66d538aeea4b25df15f175d6f8ee531d4f36b586
MaxwellGarceau <maxwellgarceau@gmail.com>
2025-01-14 12:19:26 -0500

    Simplify eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,21 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import parser from "@typescript-eslint/parser";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	{
 		files: ["**/*.{js,mjs,cjs,ts}"],
-	},
-	{
 		ignores: ["node_modules/**", "dist/**", "coverage/**"],
-	},
-	{
 		languageOptions: {
+			ecmaVersion: 2020,
+			sourceType: "module",
 			globals: {
-				...globals.browser, ...globals.node,
-			}
+				...globals.browser,
+				...globals.node,
+			},
+			parser,
 		},
 		rules: {
 			// Enforce tabs
