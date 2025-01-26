a351383c3c4fee470d5a690e8c18a130ecd87b02
Max Garceau <maxwellgarceau@gmail.com>
2025-01-26 17:16:12 -0500

    Started scaffolding index.ts and added notes for implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Plagiarism checker running");
+import { appLogger } from "./config/log4js";
+import process from "./process";
+import TFIDFRegistry from './init/tfidf';
+
+// TODO: Set defaults for args and type args
+// TODO: Make a registry following the pattern for TFIDF and import here
+// TODO: Add logging comments for each step
+// TODO: Tie all parts together
+function isPlagiarism(referenceDocument: string, queryDocument: string, args: Object = {}): boolean {
+	// Process
+	const refProcessed = process(referenceDocument);
+	const queryProcessed = process(referenceDocument);
+
+	// Extract TF-IDF
+	const tfidf = TFIDFRegistry.get('natural');
+	// TODO: How am I going to add documents here and compare?
+
+	// Compare similarity
+
+	/**
+	 * Return isPlagiarism boolean
+	 * 
+	 * TODO: If greater than 0.8 then pass as not plagiarism
+	 * TOOD: Double check that 0.8 is a good breakpoint for this conditional check
+	 */
+	return false;
+}
+
+export default isPlagiarism;
