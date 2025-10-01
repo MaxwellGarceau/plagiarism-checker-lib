c31691a1ed96881f24f87771791d22e528faf3e6
mgarceau <maxwellgarceau@Maxwells-MacBook-Air.local>
2025-10-01 16:24:18 -0400

    Add and return TFIDFExtractor explicitly

--- src/interfaces/Registry.ts
+++ src/interfaces/Registry.ts
@@ -1,18 +1,19 @@
 /**
  * Interface for managing multiple instances of an implementation.
  */
+import { TFIDFExtractor } from './TFIDFExtractor';
 export interface Registry {
 	/**
 	 * Add a new TFIDFExtractor instance to the registry.
 	 * @param id A unique identifier for the instance
 	 * @param implementation The concrete implementation
 	 */
-	add(id: string, implementation: Object): void;
+	add(id: string, implementation: TFIDFExtractor): void;
 
 	/**
 	 * Retrieve a TFIDFExtractor instance from the registry.
 	 * @param id The unique identifier of the instance
 	 * @returns The concrete implementation
 	 */
-	get(id: string): Object;
+	get(id: string): TFIDFExtractor;
 }
